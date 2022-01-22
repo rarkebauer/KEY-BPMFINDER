1a7142fe01e2ee1f73e14d8e4ae6713d5064a2d5
Alexandre Hamelin <alexandre.hamelin@gmail.com>
2022-01-22 12:30:13 -0500

    Implement changes to avoid the webflow, use client_credentials only

--- eventPage.js
+++ eventPage.js
@@ -6,12 +6,15 @@ chrome.runtime.onInstalled.addListener(function(){
   })
 })
 const redirectUri = 'https://$CHROME_TOKEN.chromiumapp.org/success'
+const clientId = 'your-api-client-id';
+const clientSecret = 'your-api-client-secret';
 
 function makeXhrPostRequest(code, grantType, refreshToken){
   return new Promise((resolve, reject) => {
     let xhr = new XMLHttpRequest();
     xhr.open('POST', 'https://accounts.spotify.com/api/token', true);
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
+    xhr.setRequestHeader('Authorization', 'Basic ' + btoa(clientId + ':' + clientSecret))
     xhr.onload = function(){
       if (xhr.status >= 200 && xhr.status < 300){
           return resolve(xhr.response);
@@ -31,12 +34,14 @@ function makeXhrPostRequest(code, grantType, refreshToken){
     }
 
      let requestBody = (refreshToken) ? 'grant_type=' + grantType + '&refresh_token=' + refreshToken + '&client_id=$SPOTIFY_CLIENT_ID&client_secret=$SPOTIFY_CLIENT_SECRET' : 'grant_type=' + grantType + '&code=' + code + '&redirect_uri=' + redirectUri + '&client_id=$SPOTIFY_CLIENT_ID&client_secret=$SPOTIFY_CLIENT_SECRET'
+    requestBody = `grant_type=${grantType}`; // experimenting with no webflow, force client_credentials
     xhr.send(requestBody)
   })
 }
 
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse){
   if (request.action === 'launchOauth'){
+/*
     chrome.identity.launchWebAuthFlow({
       url: 'https://accounts.spotify.com/authorize' +
       '?client_id=$SPOTIFY_CLIENT_ID' +
@@ -46,10 +51,13 @@ chrome.extension.onMessage.addListener(function(request, sender, sendResponse){
     },
     function(redirectUrl) {
       let code = redirectUrl.slice(redirectUrl.indexOf('=') + 1)
+*/
 
-      makeXhrPostRequest(code, 'authorization_code')
+      makeXhrPostRequest(null, 'client_credentials')
         .then(data => {
           data = JSON.parse(data)
+          // {"access_token":"tokentokentoken","token_type":"Bearer","expires_in":3600}
+          console.log(`here is your access_token: ${data.access_token} expiring at ${new Date(Date.now()+data.expires_in*1000)}`);
           chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab){
             if (
               changeInfo.status === 'complete' && tab.url.indexOf('spotify') > -1
@@ -65,7 +73,7 @@ chrome.extension.onMessage.addListener(function(request, sender, sendResponse){
           return data
         })
         .catch(err => console.error(err))
-    }) //launch web auth flow
+    //}) //launch web auth flow
 
   } //if statment
 })// extension event listener
